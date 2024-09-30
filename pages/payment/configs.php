d32b1eeabb674119e85170e0a08ae6f687a1508a
Avit Brian <b.mugisha2@alustudent.com>
2024-09-30 17:14:48 +0200

    updated private key-flutterwave

--- pages/payment/configs.php
+++ pages/payment/configs.php
@@ -1,3 +1,3 @@
 <?php
-$SecretKey = "FLWSECK_TEST-e44b1d44c80b74f4b6fe4f16b9940515-X"; 
-$PublicKey = "pk_test_a0ea251197083117f4d82fc77be0093dfa98cebb"; 
+$SecretKey = "FLWSECK_TEST-16c5b8d192f60e0dd8861df3d2f60eb9-X"; 
+$PublicKey = ""; 
